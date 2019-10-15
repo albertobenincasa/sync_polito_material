b897c7cfbf8f2d37128af1b45b8ec486b20b3565
Mirkuz93 <bertonemirko@gmail.com>
2019-10-15 23:25:57 +0200

    User can download only material

---
+++ src/login_window.py
@@ -0,0 +1,46 @@
+from PyQt5 import QtWidgets, uic, QtCore, QtGui
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from polito_sync import PolitoWebClass
+from PyQt5.QtWidgets import QMessageBox
+from subjects_window import SubjectsWindow
+
+import sys
+
+
+class LoginWindow(QtWidgets.QMainWindow):
+    def __init__(self,session):
+        super(LoginWindow, self).__init__()
+        self.session = session
+
+        uic.loadUi('login_window.ui', self)
+
+        self.LoginButton = self.findChild(QtWidgets.QPushButton, 'loginButton')  # Find the button
+        self.LoginButton.clicked.connect(self.printButtonPressed)  # Remember to pass the definition/method, not the return value!
+
+        self.username = self.findChild(QtWidgets.QLineEdit, 'lineEdit')
+        self.username.setPlaceholderText("Username")
+
+        self.pwd = self.findChild(QtWidgets.QLineEdit, 'lineEdit_2')
+        self.pwd.setPlaceholderText("Password")
+        self.pwd.setEchoMode(QtWidgets.QLineEdit.Password)
+
+        self.secondWindow = SubjectsWindow(session)
+
+
+
+    def printButtonPressed(self):
+        print('Button Pressed ' + self.username.text() + ' ' + self.pwd.text())
+        if not self.session.login(self.username.text(), self.pwd.text()):
+            self.popup_error_username_password()
+            return
+        self.secondWindow.show()
+        self.close()
+
+        #self.close()
+
+    def popup_error_username_password(self):
+        msg = QMessageBox()
+        msg.setWindowTitle("Warning!")
+        msg.setText("Email o password errati")
+        msg.setIcon(QMessageBox.Warning)
+        msg.exec_()
--- src/main.py
+++ src/main.py
@@ -1,27 +1,37 @@
-import json
-import os
-
+from PyQt5 import QtWidgets, uic, QtCore, QtGui
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from polito_sync import PolitoWebClass
-#import PolitoWebClass
+from PyQt5.QtWidgets import QMessageBox
 
-if __name__ == "__main__":
-    session = PolitoWebClass()
+import sys
+import json
+from login_window import LoginWindow
 
-    settings = None
+# def window():
+#     app = QApplication(sys.argv)
+#     win = QMainWindow()
+#     win.setGeometry(1000, 200, 300, 300)  # (x,y,w,h) --> where start(x,y) how big(w,h)
+#     win.setWindowTitle("Polito Sync Material")
+#
+#     label = QtWidgets.QLabel(win)
+#     label.setText("label")
+#     label.move(50, 50)
+#
+#     win.show()
+#     sys.exit(app.exec_())
+#
+session = PolitoWebClass()
+try:
+    with open("settings.json") as s:
+        settings = json.load(s)
+except:
+    print("Error: rename settings file as settings.json")
 
-    try:
-        with open("settings.json") as s:
-            settings = json.load(s)
-    except:
-        print("Error: rename settings file as settings.json")
+session.set_download_folder(settings['download_folder'])
 
-    session.set_download_folder(settings['download_folder'])
+session.set_file_name('web')
+app = QtWidgets.QApplication(sys.argv)
+window = LoginWindow(session)
+window.show()
 
-    session.set_file_name('web')
-
-    if settings['credentials']['enabled']:
-        session.login(settings['credentials']['username'], settings['credentials']['password'])
-    else:
-        session.login()
-
-    session.menu()
+sys.exit(app.exec_())
